fb0407cce11fe9cf83378eb77964e62058a33e3b
quiddler <elioteasterling@gmail.com>
2022-11-07 13:13:25 -0500

    set type to module

--- data_structures/tests/LinkedList.test.js
+++ data_structures/tests/LinkedList.test.js
@@ -1,3 +1,10 @@
-import LinkedList from "../LinkedList"
+import LinkedList from "../LinkedList.js"
 
 const l = new LinkedList()
+
+l.addFront(2)
+l.addFront(7)
+
+console.log(l.size)
+
+export const x = 0
