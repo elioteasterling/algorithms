292b9f558e58d03323a912206f1de04ed2eb8ff0
quiddler <elioteasterling@gmail.com>
2022-11-07 14:10:31 -0500

    linked and resizing array stack implementations

--- data_structures/Stack.js
+++ data_structures/Stack.js
@@ -1,5 +1,29 @@
-import LinkedList from "./LinkedList.js";
+import LinkedList from "./LinkedList.js"
 
-export default class Stack {
-    stack = new LinkedList()
+// quicker access but total time is slower and more memory is used 
+export class Stack {
+    list = new LinkedList()
+
+    push(value) { this.list.addFront(value) }
+
+    pop() { return this.list.removeFront() }
+
+    isEmpty() { return this.list.size === 0 }
+}
+
+// total time is better and uses less memory but quick access isn't guarenteed due to array resizing
+export class ArrayStack {
+
+    list = []
+    n = 0
+
+    push(value) { this.list[n++] = value }
+
+    pop () { 
+        const result = this.list[--n]
+        this.list[n] = null             // collect whatever's inside the array at position n
+        return result
+    }
+
+    isEmpty() { return this.n === 0 }
 }
