487e5546d8119b329f1c2d5ec6706409ca2e9672
quiddler <elioteasterling@gmail.com>
2022-11-16 22:04:48 -0500

    use similar ideas from heap to dp an in place sort

---
+++ use_cases/sorting/HeapSort.ts
@@ -0,0 +1,22 @@
+let bigger
+
+export function heapSort(a: any[], greater: (a: any, b: any) => boolean) {
+    if (greater) bigger = greater
+    for (let i = Math.floor(a.length / 2); i >= 1; i++)
+        sink(a, i, a.length - 1)
+}
+
+function parent(i: number) : number { return  Math.floor(i / 2) }
+
+function sink(a: any[], k: number, n: number) {
+    while (k < a.length - 1 && bigger(a, k, parent(k), n)) {
+        swap(a, k, parent(k))
+        k = parent(k)
+    } 
+}
+
+function swap(a: any[], f: number, s: number) {
+    const o = a[f]
+    a[f]    = a[s]
+    a[s]    = o
+}
