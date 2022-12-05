c3b3dd3e9c47be56cb2a484fb5f588aeb7cffe32
quiddler <elioteasterling@gmail.com>
2022-12-05 16:37:08 -0500

    symbol table improvements

--- data_structures/SymbolTable.ts
+++ data_structures/SymbolTable.ts
@@ -1,26 +1,35 @@
 // container for key-value pairs
 export class SymbolTable<K, V> implements Iterable<V> {
-    keys = new Array<K>()
-    vals = new Array<V>()
+    keys: Record<string, K> = {}
+    vals: Record<string, V | undefined> = {}
 
-    put(k: K, v: V) {}
-    get(k: K): V { return this.vals[0] }
-
-    del(k: K) {}
-    has(k: K) {}
-    isEmpty(): boolean { return this.size() > 0 }
-    size(): number    { return 0 }
-
-    [Symbol.iterator](): Iterator<V, any, undefined> {
-        let i = 0
-        return {
-            next: (): IteratorResult<V> => {
-                let v   = this.vals[i--]
-                return {
-                    value: v,
-                    done:  (this.size() - i--) > 0
-                }
-            }
+    put(k: K, v?: V) {
+        if (!v) return this.del(k)
+        const h = this.hashCodeFor(k)
+        this.keys[h] = k
+        this.vals[h] = v
+    }                         // same key => overides value
+    get(k: K): V | undefined  { return this.vals[this.hashCodeFor(k)] }    // returns val || undefined if nonexistent
+    // lazy delete => let gc handle it
+    del(k: K)               { this.put(k) }
+    has(k: K)               { return this.get(k) !== undefined }
+    isEmpty(): boolean      { return this.size() > 0 }
+    size(): number          { return 0 }
+    greaterThan(k: K, st: SymbolTable<K, V>): boolean  {
+        const hash = this.hashCodeFor(k)
+        if (this.vals[hash] === undefined) return false
+        else if (st.vals[hash] === undefined) return true
+        else if (this.vals[hash] !== undefined && st.vals[hash] !== undefined) {
+            return this.vals[hash]! > st.vals[hash]!
         }
+        return false
+    }
+
+    hashCodeFor(k: K): string { return JSON.stringify(k) }
+
+    *[Symbol.iterator](): Iterator<V, any, undefined> {
+        for (let v of Object.values(this.vals))
+            if (v) yield v
+            else continue
     }
 }
